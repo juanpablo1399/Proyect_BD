3e3fddeefc27c18864f88f5d83757f26fddab7bd
Juan Pablo Gutierrez <juanpablo9913@outlook.com>
2024-09-06 19:19:40 -0500

    Push clase

--- uploadfiles.py
+++ uploadfiles.py
@@ -1,11 +1,15 @@
 import streamlit as st
 import pandas as pd
 
+
+def _display_dataframe(df):
+    st.write(df)
+
 def _extract_data_from_excel1(excel_file1):
 
     try:
         df1 = pd.read_excel(excel_file1)
-        st.write(df1)  # Displays the dataframe
+        st.write(df1)
     except Exception as e:
         st.write(f"Error reading the Excel file: {e}")
         return []
@@ -26,7 +30,7 @@ def _extract_data_from_excel2(excel_file2):
     
     try:
         df2 = pd.read_excel(excel_file2)
-        st.write(df2)  # Displays the dataframe
+        st.write(df2)
     except Exception as e:
         st.write(f"Error reading the Excel file: {e}")
         return []
@@ -37,4 +41,9 @@ uploaded_file2 = st.file_uploader("Role list", type=["xls", "xlsx"], key="file_u
 if uploaded_file2 is not None:
     st.write("First file was uploaded successfully.")
     _extract_data_from_excel2(uploaded_file2)
-    
+
+if st.button("Join DataFrame"):
+    df1=pd.read_excel(uploaded_file1)
+    df2=pd.read_excel(uploaded_file2)
+    df=pd.concat([df1,df2],axis=1)
+    _display_dataframe(df)
